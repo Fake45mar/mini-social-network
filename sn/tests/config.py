a978dbcfa237ba153bab9c6e9afb6f4c4b4f7117
Fake45mar <levm975@gmail.com>
2021-01-01 17:09:09 +0200

    tests in wrong branch

--- sn/tests/config.py
+++
@@ -1,9 +0,0 @@
-url = 'http://127.0.0.1:8000/'
-requests = {
-    'signup': url + 'signup',
-    'login': url + 'login',
-    'create_post': url + 'create_post',
-    'like': url + 'like',
-    'dislike': url + 'dislike',
-    'logout': url + 'logout'
-}
